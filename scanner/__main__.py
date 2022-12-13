43bf38836822e78807f1fa39023b026870270de9
valkheim <>
2022-12-13 17:08:49 +0100

    fo

--- scanner/__main__.py
+++ scanner/__main__.py
@@ -1,22 +1,23 @@
-import flask
-import hashlib
-import os
-import json
-import time
 import datetime
+import hashlib
+import json
+import os
 
-from scanner.analyse import run_extractors, get_extractors_data
-from scanner.utils import read_result_infos, readfile, hexdump
+import flask
+
+from scanner.analyse import get_extractors_data, run_extractors
+from scanner.utils import read_result_infos
 
 app = flask.Flask(__name__)
 app.secret_key = "super secret key"
 
+
 @app.route("/", methods=["GET"])
 def index():
-    import os
-    last_results = {x:x for x in os.listdir("results")}
     last_results = []
-    results_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "results"))
+    results_dir = os.path.normpath(
+        os.path.join(os.path.dirname(__file__), "..", "results")
+    )
     for result in os.listdir(results_dir):
         if (infos := read_result_infos(result)) is not None:
             last_results += [infos]
@@ -24,51 +25,66 @@ def index():
     print(last_results)
     return flask.render_template("index.html", last_results=last_results)
 
+
 @app.route("/r/<hash>", methods=["GET"])
 def result(hash):
-    dst_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "results", hash))
+    dst_dir = os.path.normpath(
+        os.path.join(os.path.dirname(__file__), "..", "results", hash)
+    )
     results = {
         "infos": read_result_infos(hash),
-        "extractors": get_extractors_data(dst_dir)
+        "extractors": get_extractors_data(dst_dir),
     }
     return flask.render_template("index.html", results=results)
+
 
 @app.route("/a/<hash>")
 def analyse(hash):
     infos = read_result_infos(hash)
-    dst_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "results", hash))
+    dst_dir = os.path.normpath(
+        os.path.join(os.path.dirname(__file__), "..", "results", hash)
+    )
     dst_file = os.path.join(dst_dir, infos["filename"])
     run_extractors(dst_file)
     return flask.redirect(flask.url_for("result", hash=hash))
+
 
 @app.route("/upload", methods=["POST"])
 def upload():
     f = flask.request.files.get("file")
     hash = hashlib.sha1(f.read()).hexdigest()
     f.seek(0)
-    dst_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "results", hash))
+    dst_dir = os.path.normpath(
+        os.path.join(os.path.dirname(__file__), "..", "results", hash)
+    )
     dst_file = os.path.join(dst_dir, f.filename)
     if not os.path.isdir(dst_dir):
         os.mkdir(dst_dir)
         f.save(dst_file)
 
     with open(os.path.join(dst_dir, "infos.json"), "wt") as fh:
-        fh.write(json.dumps(
-            {
-            "filename": f.filename,
-            "sha1": hash,
-            "last_update": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            }
-        ))
+        fh.write(
+            json.dumps(
+                {
+                    "filename": f.filename,
+                    "sha1": hash,
+                    "last_update": datetime.datetime.now().strftime(
+                        "%Y-%m-%d %H:%M:%S"
+                    ),
+                }
+            )
+        )
 
     return flask.redirect(flask.url_for("analyse", hash=hash))
 
+
 def main():
     app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.debug = True
     app.run()
     flask.session.clear()
+
 
 if __name__ == "__main__":
     main()
--- scanner/analyse.py
+++ scanner/analyse.py
@@ -1,35 +1,63 @@
+import glob
 import os
 import subprocess
-import glob
 import typing as T
 
-def run_process(args: T.List, write: T.Optional[str] = None, stdout: T.Union[T.BinaryIO, int] = subprocess.PIPE, stderr : T.Union[T.BinaryIO, int] = subprocess.PIPE, **kwargs: T.Any):
-    p = subprocess.Popen(args, universal_newlines=True, close_fds=False,
-    stdout=stdout, stderr=stderr, **kwargs)
+
+def run_process(
+    args: T.List[str],
+    write: T.Optional[str] = None,
+    stdout: T.Union[T.BinaryIO, T.TextIO, int] = subprocess.PIPE,
+    stderr: T.Union[T.BinaryIO, T.TextIO, int] = subprocess.PIPE,
+    **kwargs: T.Any
+) -> T.Tuple[int, str, str]:
+    p = subprocess.Popen(
+        args,
+        universal_newlines=True,
+        close_fds=False,
+        stdout=stdout,
+        stderr=stderr,
+        **kwargs
+    )
     o, e = p.communicate(write)
     return p.returncode, o, e
+
 
 def yield_files(from_dir: str) -> T.Iterator[str]:
     for root, _, files in os.walk(from_dir):
         for file in files:
             yield os.path.join(os.path.basename(root), file)
 
+
 def run_extractors(filepath: str) -> None:
     extractors_dir = os.path.join(os.path.dirname(__file__), "extractors")
     for extractor_relpath in yield_files(extractors_dir):
         extractor_abspath = os.path.join(extractors_dir, extractor_relpath)
         parts = extractor_relpath.split(os.sep)
-        if any([ x.startswith('_') for x in parts]):
+        if any([x.startswith("_") for x in parts]):
             continue
 
-        results_absdir = os.path.join(os.path.dirname(filepath), *parts[:-1], ".".join(parts[-1].split('.')[:-1]))
+        results_absdir = os.path.join(
+            os.path.dirname(filepath),
+            *parts[:-1],
+            ".".join(parts[-1].split(".")[:-1])
+        )
         os.makedirs(results_absdir, exist_ok=True)
         with open(os.path.join(results_absdir, "stdout.log"), "wt") as stdout:
-            with open(os.path.join(results_absdir, "stderr.log"), "wt") as stderr:
-                run_process([extractor_abspath, filepath], stdout=stdout, stderr=stderr)
+            with open(
+                os.path.join(results_absdir, "stderr.log"), "wt"
+            ) as stderr:
+                run_process(
+                    [extractor_abspath, filepath], stdout=stdout, stderr=stderr
+                )
+
 
 def get_extractors_data(filedir: str) -> T.Dict[str, T.Any]:
-    files = [y for x in os.walk(filedir) for y in glob.glob(os.path.join(x[0], '*.log'))]
+    files = [
+        y
+        for x in os.walk(filedir)
+        for y in glob.glob(os.path.join(x[0], "*.log"))
+    ]
     ldirs = len(os.path.normpath(filedir).split(os.sep))
     results = {}
     for file in files:
@@ -38,5 +66,5 @@ def get_extractors_data(filedir: str) -> T.Dict[str, T.Any]:
             data = fh.read()
             if len(data) > 0:
                 results[subpath] = data
-    
+
     return results

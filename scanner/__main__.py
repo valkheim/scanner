b3e642fcb4ce910b6053f2d70ccc9f584316c5e4
valkheim <>
2022-12-19 09:02:26 +0100

    use subparsers

--- scanner/__main__.py
+++ scanner/__main__.py
@@ -8,18 +8,21 @@ import scanner.gui
 def get_arguments() -> argparse.Namespace:
     """Parse CLI options"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--gui", action="store_true")
-    parser.add_argument("--last", action="store_true")
-    parser.add_argument("--file")
+    subs = parser.add_subparsers()
+
+    gui_parser = subs.add_parser("gui")
+    gui_parser.set_defaults(run=scanner.gui.run)
+
+    cli_parser = subs.add_parser("cli")
+    cli_parser.set_defaults(run=scanner.cli.run)
+    cli_parser.add_argument("--file")
+
     return parser.parse_args()
 
 
 def main():
     args = get_arguments()
-    if args.gui:
-        return scanner.gui.run(args)
-
-    return scanner.cli.run(args)
+    return args.run(args)
 
 
 if __name__ == "__main__":

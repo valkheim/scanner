1ea4d0927e91c3863d541744baab35dba9f29e1a
Charles Paulet <valkheim@protonmail.com>
2023-03-07 23:17:07 +0100

    remove classifier

--- scanner/__main__.py
+++ scanner/__main__.py
@@ -1,7 +1,6 @@
 import argparse
 import sys
 
-import scanner.classify
 import scanner.cli
 import scanner.gui
 
@@ -24,42 +23,6 @@ def get_arguments() -> argparse.Namespace:
     cli_parser.add_argument(
         "--last", action="store_true", help="Retrieve the last analyzed files"
     )
-
-    cli_parser = subs.add_parser("classify")
-    cli_parser.set_defaults(run=scanner.classify.run)
-    cli_parser.add_argument(
-        "--output_dir", help="Directory path where production will be stored"
-    )
-    cli_parser.add_argument(
-        "--malwares_dir", help="Directory path of the malware samples"
-    )
-    cli_parser.add_argument(
-        "--benigns_dir", help="Directory path of the benignware samples"
-    )
-    cli_parser.add_argument(
-        "--scatter_matrix",
-        action="store_true",
-        help="Generate a scatter matrix of the analysis features",
-    )
-    cli_parser.add_argument(
-        "--correlation_matrix",
-        action="store_true",
-        help="Generate a correlation matrix of the analysis features",
-    )
-
-    cli_parser.add_argument(
-        "--classifier_path",
-        help="Path of the serialized classifier to use (e.g. /tmp/random_forest.joblib",
-    )
-    cli_parser.add_argument(
-        "--test_file", help="Predict the software class of a file by path"
-    )
-    cli_parser.add_argument(
-        "--test_dir",
-        help="Predict the software class of files within a directory by path",
-    )
-
-    cli_parser.add_argument("--dry", help="Test features")
 
     return parser, parser.parse_args()
 

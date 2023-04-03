6400513f49385438bcdd7e220b042b8461802793
Charles Paulet <valkheim@protonmail.com>
2023-04-03 23:02:31 +0200

    fix: packers output

--- scanner/extractors/pe/packers.py
+++ scanner/extractors/pe/packers.py
@@ -5,7 +5,7 @@ import sys
 from _pe import get_packers
 
 if __name__ == "__main__":
-    if (packers := get_packers(sys.argv[1])) is None:
+    if (packers := get_packers(sys.argv[1])) == []:
         sys.exit(1)
 
     if packers:

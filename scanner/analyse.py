7517f1fb5a44bd144f315974890ac73d7f0c4e51
Charles Paulet <valkheim@protonmail.com>
2023-04-04 00:15:21 +0200

    sort results

--- scanner/analyse.py
+++ scanner/analyse.py
@@ -113,9 +113,11 @@ def handle_submitted_file(f, filename: str) -> str:
 def get_results(hash: str) -> T.Dict[str, T.Any]:
     results_dir = get_results_dir()
     dst_dir = os.path.join(results_dir, hash)
+    extractors_data = get_extractors_data(dst_dir)
+    sorted_extractors_data = dict(sorted(extractors_data.items()))
     return {
         "infos": read_result_infos(hash),
-        "extractors": get_extractors_data(dst_dir),
+        "extractors": sorted_extractors_data,
     }
 
 

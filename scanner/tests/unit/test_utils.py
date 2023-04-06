c5dc783b6063902acb05797e0a51e7405853bc13
Charles Paulet <valkheim@protonmail.com>
2023-04-06 22:45:27 +0200

    add a first unit test

---
+++ scanner/tests/unit/test_utils.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+import unittest.mock
+
+from scanner import utils
+
+
+class TestUtils(unittest.TestCase):
+    @unittest.mock.patch("os.makedirs")
+    def test_get_results_dir_without_hash(self, mock):
+        package_basedir = os.path.normpath(
+            os.path.join(os.path.dirname(__file__), "..", "..")
+        )
+        expected_dir = os.path.join(package_basedir, "results")
+        self.assertEqual(utils.get_results_dir(), expected_dir)
+        mock.assert_called_with(expected_dir, exist_ok=True)
+
+    @unittest.mock.patch("os.makedirs")
+    def test_get_results_dir_with_hash(self, mock):
+        package_basedir = os.path.normpath(
+            os.path.join(os.path.dirname(__file__), "..", "..")
+        )
+        expected_dir = os.path.join(package_basedir, "results", "hash")
+        self.assertEqual(utils.get_results_dir("hash"), expected_dir)
+        mock.assert_called_with(expected_dir, exist_ok=True)
